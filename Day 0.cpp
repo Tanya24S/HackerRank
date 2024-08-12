e3dfd04411a71f06f31e34cd8c0ad70261aa61cc
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2024-08-12 11:56:06 +0530

    Create Day0.cpp

---
+++ Day 0.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string n;
+    getline(cin, n);
+    cout<< "Hello, World."<<endl;
+    cout<< n;
+    return 0;
+}
