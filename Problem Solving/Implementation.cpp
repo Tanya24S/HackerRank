ebedfc801422156984b3227a78864fbda9b7ca6f
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2024-09-11 08:15:44 +0530

    Update Implementation.cpp

--- Problem Solving/Implementation.cpp
+++ Problem Solving/Implementation.cpp
@@ -1,1 +1,18 @@
 //Grading students
+vector<int> gradingStudents(vector<int> grades) {
+    vector<int> finalgrade;
+    for(int i = 0; i < grades.size(); i++) {
+        if(grades[i] >= 38) {
+            int nextMultipleOf5 = ((grades[i] / 5) + 1) * 5;
+            if(nextMultipleOf5 - grades[i] < 3) {
+                finalgrade.push_back(nextMultipleOf5);
+            } else {
+                finalgrade.push_back(grades[i]);
+            }
+        } else {
+            finalgrade.push_back(grades[i]);
+        }
+    }
+    return finalgrade;
+}
+//Apple and Orange
