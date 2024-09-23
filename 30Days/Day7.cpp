a7c186f6c823cd21f86e6c82752692dcd132b11f
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2024-09-23 14:08:46 +0530

    Update Day7.cpp

--- 30Days/Day7.cpp
+++ 30Days/Day7.cpp
@@ -1,1 +1,53 @@
-//
+// Reverse Arrays
+void reversearray(int n,vector<int> &arr){
+    for(int i=n-1; i>=0;i--){
+        cout<< arr[i]<< " ";
+    }
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+    reversearray(n,arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
